80a67945f668f8903bedca10c1ec163b32499a15
danackerson <dan@ackerson.de>
2020-12-03 17:36:17 +0100

    now with cmds

--- telegram.go
+++ telegram.go
@@ -2,43 +2,44 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"net/url"
 	"os"
-	"strconv"
+	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
-// sendTextToTelegramChat sends a text message to the Telegram chat identified by its chat Id
-func sendTextToTelegramChat(chatID int64, text string) (string, error) {
-	log.Printf("Sending %s to chat_id: %d", text, chatID)
-	var telegramAPI string = "https://api.telegram.org/bot" +
-		os.Getenv("CTX_TELEGRAM_BOT_TOKEN") + "/sendMessage"
-	response, err := http.PostForm(
-		telegramAPI,
-		url.Values{
-			"chat_id": {strconv.FormatInt(chatID, 10)},
-			"text":    {text},
-		})
+func pollForMessages(bot *tgbotapi.BotAPI, updates tgbotapi.UpdatesChannel) {
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
 
-	if err != nil {
-		log.Printf("error when posting text to the chat: %s", err.Error())
-		return "", err
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+		if update.Message.IsCommand() {
+			switch update.Message.Command() {
+			case "help":
+				msg.Text = "type /sayhi or /status."
+			case "sayhi":
+				msg.Text = "Hi :)"
+			case "status":
+				msg.Text = "I'm ok."
+			case "withArgument":
+				msg.Text = "You supplied the following argument: " + update.Message.CommandArguments()
+			case "html":
+				msg.ParseMode = "html"
+				msg.Text = "This will be interpreted as HTML, click <a href=\"https://www.example.com\">here</a>"
+			default:
+				msg.Text = "I don't know that command"
+			}
+		} else {
+			msg.Text = fmt.Sprintf("%s said '%s'\n", update.Message.From, update.Message.Text)
+		}
+		bot.Send(msg)
 	}
-	defer response.Body.Close()
-
-	var bodyBytes, errRead = ioutil.ReadAll(response.Body)
-	if errRead != nil {
-		log.Printf("error in parsing telegram answer %s", errRead.Error())
-		return "", err
-	}
-	bodyString := string(bodyBytes)
-	log.Printf("Body of Telegram Response: %s", bodyString)
-
-	return bodyString, nil
 }
 
 func main() {
@@ -61,15 +62,12 @@ func main() {
 		log.Fatal(err)
 	}
 	if info.LastErrorDate != 0 {
-		log.Printf("Telegram callback failed: %s", info.LastErrorMessage)
+		log.Printf("Telegram callback last failure @ %s: %s",
+			time.Unix(int64(info.LastErrorDate), 0),
+			info.LastErrorMessage)
 	}
 	updates := bot.ListenForWebhook("/" + bot.Token)
 	go http.ListenAndServe("0.0.0.0:3000", nil)
 
-	for update := range updates {
-		log.Printf("%s wrote '%s'\n", update.Message.From, update.Message.Text)
-		response := fmt.Sprintf("good copy %s. rcvd %s\n",
-			update.Message.From, update.Message.Text)
-		sendTextToTelegramChat(update.Message.Chat.ID, response)
-	}
+	pollForMessages(bot, updates)
 }
